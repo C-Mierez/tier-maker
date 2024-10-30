e310cf6c53af2394202414f676b21992abc1e4b6
C-Mierez <carlos.mierez20@gmail.com>
2024-10-30 20:55:51 -0300

    ✨ feat: Implement tier list layout

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import "./style.css";
+
+// Constants
+const TierColorsVars = ["--color-s", "--color-a", "--color-b", "--color-c", "--color-d", "--color-e", "--color-f"];
+
+// Elements
+const $tierList = document.querySelector(".list") as HTMLDivElement;
+const $tiers = document.querySelectorAll(".tier") as NodeListOf<HTMLDivElement>;
+
+function start() {
+    colorTiers();
+}
+
+function colorTiers() {
+    $tiers.forEach(($tier, index) => {
+        $tier.style.setProperty("--color", `var(${TierColorsVars[index]})`);
+    });
+}
+
+start();
