c3a6a848c3911b7629bef5114f319dd7520121dd
C-Mierez <carlos.mierez20@gmail.com>
2024-10-30 22:57:24 -0300

    ✨ feat: Tier list functionality with drag-and-drop support and image upload

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,168 @@
 import "./style.css";
+import { $, $$ } from "./utils";
 
 // Constants
 const TierColorsVars = ["--color-s", "--color-a", "--color-b", "--color-c", "--color-d", "--color-e", "--color-f"];
 
 // Elements
-const $tierList = document.querySelector(".list") as HTMLDivElement;
-const $tiers = document.querySelectorAll(".tier") as NodeListOf<HTMLDivElement>;
+const $tierList = $(".list") as HTMLDivElement;
+const $tiers = $$(".tier") as NodeListOf<HTMLDivElement>;
+const $imageInput = $("#image-input") as HTMLInputElement;
+const $selectionItems = $("#selection-items") as HTMLDivElement;
 
+// Drag Behaviour
+let draggedItem: HTMLLIElement | null = null;
+let draggedPreview: HTMLLIElement | null = null;
+let sourceContainer: HTMLDivElement | null = null;
+
+/* -------------------------------- Functions ------------------------------- */
 function start() {
     colorTiers();
+    bindEvents();
 }
 
 function colorTiers() {
     $tiers.forEach(($tier, index) => {
         $tier.style.setProperty("--color", `var(${TierColorsVars[index]})`);
     });
+}
+
+function bindEvents() {
+    // Input image load
+    $imageInput.addEventListener("change", handleImageChange);
+
+    // Tier row drag events
+    $tiers.forEach(($tier) => {
+        $tier.addEventListener("drop", handleTierDrop);
+        $tier.addEventListener("dragover", handleTierDragOver);
+        $tier.addEventListener("dragleave", handleTierDragLeave);
+    });
+
+    // Selection items drag events
+    $selectionItems.addEventListener("drop", handleTierDrop);
+    $selectionItems.addEventListener("dragover", handleTierDragOver);
+    $selectionItems.addEventListener("dragleave", handleTierDragLeave);
+}
+
+function _createNewItem(src: string) {
+    const $newLi = document.createElement("li");
+    $newLi.classList.add("tier-li");
+
+    const $newImg = document.createElement("img");
+    $newImg.src = src;
+    $newImg.alt = "Uploaded Image";
+    // Draggable properties
+    $newLi.draggable = true;
+    $newLi.addEventListener("dragstart", handleItemDragStart);
+    $newLi.addEventListener("dragend", handleItemDragEnd);
+
+    $newLi.appendChild($newImg);
+
+    return $newLi;
+}
+
+/* -------------------------------- Handlers -------------------------------- */
+function handleImageChange(e: Event) {
+    const target = e.target;
+    if (!(target instanceof HTMLInputElement)) return;
+
+    if (target.files) {
+        [...target.files].forEach((file) => {
+            const reader = new FileReader();
+
+            reader.onload = (readerEvent) => {
+                const result = readerEvent.target?.result;
+                if (typeof result === "string") {
+                    $selectionItems.appendChild(_createNewItem(result));
+                }
+            };
+
+            reader.readAsDataURL(file);
+        });
+    }
+}
+
+function handleItemDragStart(e: DragEvent) {
+    const target = e.target;
+    if (!(target instanceof HTMLLIElement)) return;
+
+    draggedItem = target;
+    sourceContainer = target.parentElement as HTMLDivElement;
+
+    // Get src from child img
+    const img = target.querySelector("img");
+    if (!img) return;
+
+    e.dataTransfer?.setData("text/plain", img.src);
+}
+
+function handleItemDragEnd(e: DragEvent) {
+    const target = e.target;
+    if (!(target instanceof HTMLLIElement)) return;
+
+    draggedItem = null;
+    sourceContainer = null;
+}
+
+function handleTierDrop(e: DragEvent) {
+    e.preventDefault();
+    const target = e.target as HTMLDivElement;
+    if (!target.getAttribute("data-droppable")) return;
+
+    if (draggedItem && sourceContainer) {
+        if (sourceContainer === target) return;
+
+        sourceContainer.removeChild(draggedItem);
+
+        const src = e.dataTransfer?.getData("text/plain");
+        if (!src) return;
+        const $newLi = _createNewItem(src);
+        target.appendChild($newLi);
+
+        target.classList.remove("drag-over");
+    }
+
+    if (draggedItem && draggedPreview) {
+        target.querySelector(".drag-preview")?.remove();
+        draggedPreview = null;
+    }
+}
+
+function handleTierDragOver(e: DragEvent) {
+    e.preventDefault();
+
+    const target = e.target as HTMLDivElement;
+    if (!target.getAttribute("data-droppable")) return;
+
+    if (draggedItem && sourceContainer) {
+        if (sourceContainer === target) return;
+
+        target.classList.add("drag-over");
+    }
+
+    if (draggedItem && !draggedPreview) {
+        draggedPreview = draggedItem.cloneNode(true) as HTMLLIElement;
+        draggedPreview.classList.add("drag-preview");
+        target.appendChild(draggedPreview);
+    }
+}
+
+function handleTierDragLeave(e: DragEvent) {
+    e.preventDefault();
+
+    const target = e.target as HTMLDivElement;
+    if (!target.getAttribute("data-droppable")) return;
+
+    if (draggedItem && sourceContainer) {
+        if (sourceContainer === target) return;
+
+        target.classList.remove("drag-over");
+    }
+
+    if (draggedItem && draggedPreview) {
+        target.querySelector(".drag-preview")?.remove();
+        draggedPreview = null;
+    }
 }
 
 start();
